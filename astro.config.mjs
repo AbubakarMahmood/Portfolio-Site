059396a791b32f25a23798878be9f88c2a0a24f4
Abubakar Mahmood <theabubakar321@gmail.com>
2026-05-19 17:01:21 +0500

    Set canonical site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://abubakarmahmood.dev',
+  site: 'https://abubakarmahmood.com',
 });
